bd05d632a69459d8c3d8548c383a11a18ff65e4f
kiyosnguyen5 <24100259@st.phenikaa-uni.edu.vn>
2025-06-23 10:32:24 +0000

    just a lecture

--- 23062025/23062025.java
+++
@@ -1,8 +0,0 @@
-public class Number {
-    public int i;
-
-}
-
-public class Number {
-    public int i
-}
--- 23062025/Number.java
+++ 23062025/Number.java
@@ -1,13 +1,3 @@
 public class Number {
     public int i;
-
-    public static void main(String[] args) {
-        Number n1 = new Number();
-        Number n2 = new Number();
-        n1.i = 2;
-        n2.i = 5;
-        n2.i = 10;
-        System.out.println(n2.i);
-        System.out.println(n1.i);
-    }
 }
